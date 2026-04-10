77e15eea00a64ee41d4ade0079613c5924830917
Boipelo Tidimalo Matlou <boipelotidimalomatlou@gmail.com>
2026-04-10 21:19:17 +0200

    Update WeightConverter.java

--- src/main/java/Util/WeightConverter.java
+++ src/main/java/Util/WeightConverter.java
@@ -6,12 +6,12 @@ Provides reusable static helper methods for unit conversion.
 
 public class WeightConverter {
 
-    // 3.1.3 - Converts kilograms to metric tons
+    // Converts kilograms to metric tons
     public static double kgToTons(double kg) {
         return kg / 1000.0;
     }
 
-    // 3.1.3 - Converts metric tons back to kilograms
+    // Converts metric tons back to kilograms
     public static double tonsToKg(double tons) {
         return tons * 1000.0;
     }
